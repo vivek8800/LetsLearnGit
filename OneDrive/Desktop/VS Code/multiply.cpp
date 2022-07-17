24efeac48e117d80e8db21f3ebcebbf1e4a60010
Vivek Kumar <v.kumar533359@gmail.com>
2022-07-17 23:25:48 +0530

    initial commit

---
+++ OneDrive/Desktop/VS Code/multiply.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+int num1,num2,product;
+   //double num1, num2, product;
+  cout << "Enter two numbers: ";
+
+  // stores two floating point numbers in num1 and num2 respectively
+  cin >> num1 >> num2;
+ 
+  // performs multiplication and stores the result in product variable
+  product = num1 * num2;  
+
+  cout << "Product is  = " << product; 
+  //cout<<"Hello Vivek!!!!"<<endl;
+
+    return 0;
+}
